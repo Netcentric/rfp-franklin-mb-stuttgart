8ae62856e245bee15c924abb3c6394a8c9253f16
Marko Vukicevic <marko.vukicevic@netcentric.biz>
2023-07-20 17:06:17 +0200

    Hero block styles, Advantages block - WIP

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,11 +21,14 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
+  const h5 = main.querySelector('h5');
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    const pictureGradient = document.createElement('div');
+    pictureGradient.classList.add('picture-gradient');
+    section.append(buildBlock('hero', { elems: [picture, pictureGradient, h1, h5] }));
     main.prepend(section);
   }
 }
