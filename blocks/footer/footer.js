d9f0e2d6fab36f3d350284172a8d6294541a78fd
Rafael Martins <rafael.martins@netcentric.biz>
2023-07-24 10:50:01 +0200

    Adds footer scroll-to-top button

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,34 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+const arrowTopIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10.566 9.192l1.414-1.414 7.071 7.07-1.414 1.415z"></path><path d="M4.949 14.808l7.07-7.07 1.415 1.414-7.07 7.07z"></path></svg>`;
+
+function getScrollToTopBlock() {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('scrollToTop__wrapper');
+
+  const button = document.createElement('button');
+  button.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+
+  });
+
+  button.innerHTML = `
+    <div>
+      <div>
+        ${arrowTopIcon}
+      </div>
+      <div><span>Nach Oben</span></div>
+    </div>
+    `;
+
+    wrapper.append(button);
+
+    return wrapper;
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -20,6 +49,10 @@ export default async function decorate(block) {
     footer.innerHTML = html;
 
     decorateIcons(footer);
+
+    const scrollToTopBlock = getScrollToTopBlock();
+    footer.prepend(scrollToTopBlock);
+
     block.append(footer);
   }
 }
